4247fa561c879db9ea175133965b40c651df2a94
Benji Mirschel <cliff.mirschel@gmail.com>
2019-08-01 09:22:57 -0400

    Destructed props and cleaned up surf forecast

--- src/components/SurfForecast.js
+++ src/components/SurfForecast.js
@@ -1,49 +1,51 @@
-import React from 'react';
+import React from "react";
 
-const tempConversion = (val) => {
+const tempConversion = val => {
   //celsius to F
 
-  return Math.round(val * 9/5 + 32);
+  return Math.round((val * 9) / 5 + 32);
+};
+
+function metersToFeet(val) {
+  return Math.round(val / 0.3048);
 }
 
-function metersToFeet (val) {
-  return Math.round(val/0.3048);
+function windSpeedConversion(val) {
+  return Math.round((val * 25) / 11);
 }
 
-function windSpeed (val) {
-  return Math.round(val * 25/11);
-}
-
-const SurfForecast = (props) => {
-  if (!props.forecastData.waveHeight){
+const SurfForecast = ({
+  forecastData: {
+    waveHeight,
+    wavePeriod,
+    swellHeight,
+    windSpeed,
+    windDirection,
+    airTemperature
+  },
+  getSwellData,
+  findSpot
+}) => {
+  if (!waveHeight) {
     return (
-      <div >
-      <h1>Enter data</h1>
-    </div>
-    )
+      <div>
+        <h1>Enter data</h1>
+      </div>
+    );
   } else {
     return (
-      <div> 
-        <button onClick={props.getSwellData}>Api</button>
-        <button onClick={props.findSpot} >Spot</button>
-        <p>Wave Height: {`${metersToFeet(props.forecastData.waveHeight)}ft`}</p>
-        <p>wavePeriod: {props.forecastData.wavePeriod}</p>
-        <p>swellHeight: {props.forecastData.swellHeight}</p>
-        <p>windSpeed: {windSpeed(props.forecastData.windSpeed)}</p>
-        <p>windDirection: {props.forecastData.windDirection}</p>
-        <p>airTemperature: {tempConversion(props.forecastData.airTemperature)}</p>
+      <div>
+        <button onClick={getSwellData}>Api</button>
+        <button onClick={findSpot}>Spot</button>
+        <p>Wave Height: {`${metersToFeet(waveHeight)}ft`}</p>
+        <p>wavePeriod: {wavePeriod}</p>
+        <p>swellHeight: {`${metersToFeet(swellHeight)}ft`}</p>
+        <p>windSpeed: {`${windSpeedConversion(windSpeed)} mph`}</p>
+        <p>windDirection: {windDirection}</p>
+        <p>airTemperature: {tempConversion(airTemperature)}</p>
       </div>
-      /*swellHeight : null,
-      waveHeight : null,
-      wavePeriod : null,
-      windSpeed : null,
-      windDirection : null,
-      waterTemperature: null,
-      airTemperature : null
-      */
     );
   }
-  
-}
+};
 
 export default SurfForecast;
